d7b895da0eab528484a90b1ec34ac29bdcf26b5f
Sheldon Lynn <sheldonlynn@outlook.com>
2024-01-23 18:36:21 -0800

    nvm, removed comments from ToastProvider. they're annoying lol

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Toast from "../Toast";
 
-export const ToastContext = React.createContext();                  // create context and export it as named export
+export const ToastContext = React.createContext();
 
-function ToastProvider({ children }) {                              // don't forget children!!!
+function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
-  const createToast = React.useCallback((variant, message) => {     // memoize any functions to avoid extra renders on pure components that consume the function
+  const createToast = React.useCallback((variant, message) => {
     const newToast = {
       id: crypto.randomUUID(),
       variant: variant,
       message: message,
     };
-    setToasts((currToasts) => [...currToasts, newToast]);           // use callback form of setState, otherwise will get stale values
-  }, []);                                                           // since using callback form, no need to add anything to dependency array
+    setToasts((currToasts) => [...currToasts, newToast]);
+  }, []);
 
   const dismissToast = React.useCallback((toastId) => {
     setToasts((currToasts) => {
@@ -22,13 +22,13 @@ function ToastProvider({ children }) {                              // don't for
     });
   }, []);
 
-  const value = React.useMemo(() => {                               // memoize the value object so it doesn't get re-generated every single re-render, 
-  return { toasts, createToast, dismissToast };                     // causing everything that consumes this Provider to re-render 
+  const value = React.useMemo(() => {
+    return { toasts, createToast, dismissToast };
   }, [toasts]);
 
   return (
-    <ToastContext.Provider value={value}>{children}</ToastContext.Provider>             // use the Provider here and wrap children
+    <ToastContext.Provider value={value}>{children}</ToastContext.Provider>
   );
 }
 
-export default ToastProvider;                                       // export the Provider component as default export
+export default ToastProvider;
